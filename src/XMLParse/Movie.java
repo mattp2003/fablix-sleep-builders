f20a8a7e3cab3749fce76f4fc18ce934eec0dbac
williamzchu@gmail.com <williamzchu@gmail.com>
2024-05-12 01:11:39 -0700

    Parser

---
+++ src/XMLParse/Movie.java
@@ -0,0 +1,34 @@
+package XMLParse;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private final String title;
+    private final String year;
+    private final String director;
+    private ArrayList<String> genres;
+
+    public Movie (String title, String year, String director, ArrayList<String> genres){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.genres = genres;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear(){
+        return year;
+    }
+
+    public String getDirector(){
+        return director;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+}
---
+++ src/XMLParse/Star.java
@@ -0,0 +1,23 @@
+package XMLParse;
+
+import java.util.ArrayList;
+
+public class Star {
+    private final String name;
+    private final String year;
+
+    public Star (String name, String year){
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear(){
+        return year;
+    }
+
+
+}
